9a131ef918830f6d514acf8a33516599472ffbaf
Andres Campuzano <campuxano88@gmail.com>
2020-09-02 09:25:15 -0500

    Update and Create working.

--- index.js
+++ index.js
@@ -19,14 +19,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({ name: Joi.string().min(3).required() });
-    const validation = schema.validate(req.body);
-    // console.log(validation);
-    if (validation.error) {
-        // 400 Bad request
-        res.status(400).send(validation.error.details[0].message);
+    const { error } = validateCourse(req.body); // the same than: validation.error
+    if (error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -35,11 +33,38 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+    // >>> Look up the course
+    // If not existing, return  404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send('Course with the given ID was now found');
+    }
+
+    // >>> Validate
+    // If invalid, return 400 - Bad request
+    const { error } = validateCourse(req.body); // the same than: validation.error
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    // >>> Update course
+    course.name = req.body.name;
+    // Return updated course
+    res.send(course);
+});
+
+// Validation function
+function validateCourse(course) {
+    const schema = Joi.object({ name: Joi.string().min(3).required() });
+    return schema.validate(course);
+}
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) {
-        // 404
-        res.status(404).send('Course now found');
+        res.status(404).send('Course with the given ID was now found');
     }
     res.send(course);
 });
